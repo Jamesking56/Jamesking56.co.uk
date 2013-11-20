7c453c451a8b62b246efad3dba6752bbebb3e51a
James King <james@jamesking56.co.uk>
2013-11-20 18:17:24 +0000

    Remove Google Analytics due to it already being added in CloudFlare

--- index.php
+++ index.php
@@ -262,19 +262,6 @@ $years = abs(13 - $age);
 	<script type="text/javascript" src="assets/js/jquery.ui.map.full.min.js?ver=3.0"></script>
 	<script type="text/javascript" src="assets/js/jquery.scrollTo-1.4.3.1-min.js"></script>
 	<script type="text/javascript" src="assets/js/jquery.localscroll-1.2.7-min.js"></script>	
-	<script type="text/javascript" src="assets/js/main.js"></script>
-
-	<!-- Google Analytics -->
-	<script type="text/javascript">
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-3000159-32']);
-	_gaq.push(['_trackPageview']);
-
-	(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-	</script>
+	<script type="text/javascript" src="assets/js/main.js"></script>s
 </body>
 </html>
